4c6cbf9c9f5e99a8def1a6cb8d659278b060da38
Vitalii Zakharchuk <vit.zkh@gmail.com>
2024-01-17 02:46:43 +0200

    left products

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ connectToDb((err) => {
 
 
 // ROUTES
-app.get('/shop-lists', (req, res) => {
+app.get('/shop-list', (req, res) => {
 	const shopLists = []
 
 	db
@@ -37,17 +37,78 @@ app.get('/shop-lists', (req, res) => {
 	}).catch(() => res.status(500).json({error: 'DB IN FIRE!!!!!'}))
 });
 
-app.post('/shop-lists', async(req, res) => {
+app.post('/shop-list', async(req, res) => {
 	await db.collection('shoplists').deleteMany({});
 
 	if(req.body.shopList.length > 0) {
 		db
 		.collection('shoplists')
 		.insertMany(req.body.shopList)
-		.then((result) => {
+		.then(() => {
 			res.status(200).json({shopLists: req.body.shopList, message: 'Список збережено!'})
 		}).catch(() => res.status(500).json({error: 'DB IN FIRE!!!!!'}))
 	} else {
 		res.status(200).json({shopLists: [], message: 'Список оновлено'})
 	}
 })
+
+app.get('/current-day', (req, res) => {
+	let currentDay = {}
+
+	db
+	.collection('currentday')
+	.find()
+	.forEach((list) => {
+		currentDay = list
+	})
+	.then(() => {
+		res.status(200).json(currentDay)
+	}).catch(() => res.status(500).json({error: 'DB IN FIRE!!!!!'}))
+});
+
+app.post('/current-day', async (req, res) => {
+	await db.collection('currentday').deleteMany({});
+
+	db
+	.collection('currentday')
+	.insertMany([req.body.currentDay])
+	.then(() => {
+		return db.collection('currentday').findOne();
+	})
+	.then((updatedData) => {
+		res.status(200).json({currentDay: updatedData, message: 'Меню на день змінено!'})
+	}).catch(() => res.status(500).json({error: 'DB IN FIRE!!!!!'}))
+
+});
+
+app.get('/left-products', (req, res) => {
+	let leftProducts = {}
+
+	db
+	.collection('leftproducts')
+	.find()
+	.forEach((list) => {
+		leftProducts = list
+	})
+	.then(() => {
+		res.status(200).json(leftProducts)
+	}).catch(() => res.status(500).json({error: 'DB IN FIRE!!!!!'}))
+});
+
+app.patch('/left-products', (req, res) => {
+	let update = ''
+	const key = Object.keys(req.body.changeProduct)[0]
+	update += key + '.howMuch'
+
+	const updateQuery = { [update]: req.body.changeProduct[key].howMuch };
+
+	db.collection('leftproducts')
+	.updateOne({}, { $set: updateQuery })
+	.then(() => {
+		return db.collection('leftproducts').findOne();
+	})
+	.then((updatedData) => {
+		res.status(200).json(updatedData);
+	})
+	.catch(() => res.status(500).json({ error: 'DB IN FIRE!!!!!' }));
+});
